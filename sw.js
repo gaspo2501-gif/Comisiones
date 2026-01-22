a6d61826df5c9e5cba91664a73a23cd746227d11
gaspo2501-gif <gaspo2501@gmail.com>
2026-01-22 16:33:51 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,54 @@
+
+const CACHE_NAME = 'comisapp-v2.4';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://esm.sh/react@19.0.0',
+  'https://esm.sh/react-dom@19.0.0/client',
+  'https://esm.sh/htm'
+];
+
+// Instalar Service Worker y cachear activos
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activar y limpiar caches antiguos
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Estrategia de respuesta: Cache first, then Network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).then((fetchResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          // Opcional: cachear nuevos recursos dinámicamente
+          if (event.request.url.startsWith('http')) {
+            cache.put(event.request, fetchResponse.clone());
+          }
+          return fetchResponse;
+        });
+      });
+    }).catch(() => {
+      // Si falla todo (offline y no en cache), retornar la página principal
+      if (event.request.mode === 'navigate') {
+        return caches.match('./index.html');
+      }
+    })
+  );
+});
