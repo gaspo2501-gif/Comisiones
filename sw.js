13ef29c9dce1f189a743459ea004b714908b915d
gaspo2501-gif <gaspo2501@gmail.com>
2026-02-09 17:17:24 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'comisapp-v2.4';
+const CACHE_NAME = 'comisapp-v3.0';
 const ASSETS = [
   './',
   './index.html',
@@ -10,34 +10,26 @@ const ASSETS = [
   'https://esm.sh/htm'
 ];
 
-// Instalar Service Worker y cachear activos
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
-// Activar y limpiar caches antiguos
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+    ))
   );
 });
 
-// Estrategia de respuesta: Cache first, then Network
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request).then((fetchResponse) => {
         return caches.open(CACHE_NAME).then((cache) => {
-          // Opcional: cachear nuevos recursos dinámicamente
           if (event.request.url.startsWith('http')) {
             cache.put(event.request, fetchResponse.clone());
           }
@@ -45,7 +37,6 @@ self.addEventListener('fetch', (event) => {
         });
       });
     }).catch(() => {
-      // Si falla todo (offline y no en cache), retornar la página principal
       if (event.request.mode === 'navigate') {
         return caches.match('./index.html');
       }
